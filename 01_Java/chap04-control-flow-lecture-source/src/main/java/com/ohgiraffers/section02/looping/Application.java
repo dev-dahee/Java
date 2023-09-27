1e05e15ac5891e66f5071873c7ea994a75adc4aa
seungje-yu <coffit23@gmail.com>
2023-09-27 12:19:47 +0900

    반복문 연습

--- 01_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
+++ 01_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -4,7 +4,11 @@ public class Application {
 
     public static void main(String[] args) {
 
-
+        A_for a = new A_for();
+//        a.testSimpleForStatement();
+//        a.testForExample1();
+//        a.testForExample2();
+        a.testForExample3();
     }
 
 }
