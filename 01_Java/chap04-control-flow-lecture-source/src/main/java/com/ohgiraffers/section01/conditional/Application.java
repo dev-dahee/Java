c3ec4f4191ccf854d1f0c432082321165381fc41
seungje-yu <coffit23@gmail.com>
2023-09-27 10:20:33 +0900

    switch 전

--- 01_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/Application.java
+++ 01_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/Application.java
@@ -11,7 +11,11 @@ public class Application {
 
         B_ifElse b = new B_ifElse();
 //        b.testSimpleIfElseStatement();
-        b.testNestedIfElseStatement();
+//        b.testNestedIfElseStatement();
+
+        C_ifElseIf c = new C_ifElseIf();
+//        c.testSimpleIfElseIfStatement();
+        c.testNestedIfElseIfStatement();
     }
 
 }
---
+++ 01_Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/D_switch.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section01.conditional;
+
+public class D_switch {
+
+    /*
+    * [switch문 표현식]
+    *
+    * switch(비교할변수) {
+    *     case 비교값1 : 비교값1과 일치하는 경우 실행할 구문; break;
+    *     case 비교값2 : 비교값2와 일치하는 경우 실행할 구문; break;
+    *     default : case에 모두 해당하지 않는 경우 실행할 구문; break;
+    * }
+    *
+    * 
+    *
+    * */
+}
